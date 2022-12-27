6a1edc0f31774dbf761f5155d08e931851e045ec
Maksim Iljushkin <m.iljushkin2@yandex.ru>
2022-12-27 16:38:54 +0300

    Added PlayMusic component

---
+++ Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayMusic : MonoBehaviour
+    {
+        [SerializeField] private AudioClip _clip;
+        private AudioSource _source;
+
+        public void Play()
+        {
+            if (_source == null)
+                _source = GameObject.FindObjectOfType<AudioSource>();
+
+            _source.PlayOneShot(_clip);
+        }
+    }
+}
